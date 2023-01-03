df9332311f265247dba9cef4eb70c01c138a3a40
Your Name <you@example.com>
2023-01-03 17:02:05 +0500

    add logging

--- src/main/java/BalanceService.java
+++ src/main/java/BalanceService.java
@@ -1,8 +1,11 @@
+import com.sun.source.util.SourcePositions;
+
 public class BalanceService {
 
     private int balance = 0;
 
     public void add(int count) {
+        System.out.println("log 1");
         balance += count;
     }
     public void withdraw(int count) {
