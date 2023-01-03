3f7f6a361f186ee4b42fe921a51e4e4d2f8b69f7
Your Name <you@example.com>
2023-01-03 16:57:35 +0500

    add some shit idk

---
+++ src/main/java/BalanceService.java
@@ -0,0 +1,15 @@
+public class BalanceService {
+
+    private int balance = 0;
+
+    public void add(int count) {
+        balance += count;
+    }
+    public void withdraw(int count) {
+        balance -= count;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
